afcabecb178b5bd58242e533f0be543601fcd4fa
PauloBCunha <161364024+PauloBCunha@users.noreply.github.com>
2024-02-26 15:31:08 -0300

    Main

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+include "settings.php";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0014)about:internet -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title><?php echo($tittle) ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+	
+	td img {
+		display: block;
+	}
+	
+	</style>
+<!--Fireworks CS6 Dreamweaver CS6 target.  Created Thu Dec 28 09:46:27 GMT-0300 (Hora oficial do Brasil) 2023-->
+</head>
+<body bgcolor="#E3D9CB">
+	<div align="center">
+<table style="display: inline-table;" border="0" cellpadding="0" cellspacing="0" width="800">
+<!-- fwtable fwsrc="Cantinho Nerd (2).png" fwpage="Page 1" fwbase="index.png" fwstyle="Dreamweaver" fwdocid = "1088823224" fwnested="0" -->
+  <tr>
+   <td><img src="images/spacer.gif" width="69" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="67" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="21" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="207" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="72" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="233" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="37" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="41" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="53" height="1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="1" alt="" /></td>
+  </tr>
+
+  <tr>
+   <td colspan="9"><img name="index_r1_c1" src="images/index_r1_c1.png" width="800" height="548" id="index_r1_c1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="548" alt="" /></td>
+  </tr>
+  <tr>
+   <td colspan="9"><img name="index_r2_c1" src="images/index_r2_c1.png" width="800" height="95" id="index_r2_c1" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="95" alt="" /></td>
+  </tr>
+  <tr>
+   <td colspan="4"><img name="index_r3_c1" src="images/index_r3_c1.png" width="364" height="61" id="index_r3_c1" alt="" /></td>
+   <td><img name="index_r3_c5" src="images/index_r3_c5.png" width="72" height="61" id="index_r3_c5" alt="" /></td>
+   <td colspan="4"><img name="index_r3_c6" src="images/index_r3_c6.png" width="364" height="61" id="index_r3_c6" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="61" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="2" colspan="2"><img name="index_r4_c1" src="images/index_r4_c1.png" width="136" height="39" id="index_r4_c1" alt="" /></td>
+   <td colspan="6"><img name="index_r4_c3" src="images/index_r4_c3.png" width="611" height="20" id="index_r4_c3" alt="" /></td>
+   <td rowspan="9"><img name="index_r4_c9" src="images/index_r4_c9.png" width="53" height="546" id="index_r4_c9" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="20" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="7"><img name="index_r5_c3" src="images/index_r5_c3.png" width="21" height="495" id="index_r5_c3" alt="" /></td>
+   <td rowspan="7" colspan="3"><img name="index_r5_c4" src="images/index_r5_c4.png" width="512" height="495" id="index_r5_c4" alt="" /></td>
+   <td rowspan="7"><img name="index_r5_c7" src="images/index_r5_c7.png" width="37" height="495" id="index_r5_c7" alt="" /></td>
+   <td rowspan="7"><img name="index_r5_c8" src="images/index_r5_c8.png" width="41" height="495" id="index_r5_c8" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="19" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="7"><img name="index_r6_c1" src="images/index_r6_c1.png" width="69" height="507" id="index_r6_c1" alt="" /></td>
+   <td><img name="index_r6_c2" src="images/index_r6_c2.png" width="67" height="60" id="index_r6_c2" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="60" alt="" /></td>
+  </tr>
+  <tr>
+   <td><img name="index_r7_c2" src="images/index_r7_c2.png" width="67" height="60" id="index_r7_c2" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="60" alt="" /></td>
+  </tr>
+  <tr>
+   <td><img name="index_r8_c2" src="images/index_r8_c2.png" width="67" height="59" id="index_r8_c2" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="59" alt="" /></td>
+  </tr>
+  <tr>
+   <td><img name="index_r9_c2" src="images/index_r9_c2.png" width="67" height="61" id="index_r9_c2" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="61" alt="" /></td>
+  </tr>
+  <tr>
+   <td><img name="index_r10_c2" src="images/index_r10_c2.png" width="67" height="62" id="index_r10_c2" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="62" alt="" /></td>
+  </tr>
+  <tr>
+   <td rowspan="2"><img name="index_r11_c2" src="images/index_r11_c2.png" width="67" height="205" id="index_r11_c2" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="174" alt="" /></td>
+  </tr>
+  <tr>
+   <td colspan="6"><img name="index_r12_c3" src="images/index_r12_c3.png" width="611" height="31" id="index_r12_c3" alt="" /></td>
+   <td><img src="images/spacer.gif" width="1" height="31" alt="" /></td>
+  </tr>
+	</table>
+	</div>
+</body>
+</html>
---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$tittle = "Cantinho Nerd - Aqui você pode ser o que quiser!!";
+
+$RPGMesa1 = "Rumo ao Desespero";
+$RPGMesa2 = "A Ascensão do Lorde das Sombras";
+$RPGMesa3 = "Oneshots";
+$gameurl = "rpg.cantinhonerdjogos.com.br";
+
+?>
